7e27e40fa5422ca0bc157aabb83a89d83e0fa3c5
Kody Carling <kodycarling@workfront.com>
2020-07-09 17:45:48 -0600

    Finished challenge 2 for the checkboxes

--- elements/pages/checkboxes_page.py
+++ elements/pages/checkboxes_page.py
@@ -9,16 +9,30 @@ class Checkboxes:
     def go_to_demoqa_checkbox(self):
         self.py.visit('https://demoqa.com/checkbox')
 
-    def expand_home_toggle_first_time(self):
+    def expand_first_toggle(self):
         self.py.get("button[title='Toggle']").click()
 
-    def expand_toggles(self, index):
+    def expand_all(self):
+        self.expand_first_toggle()
+        self.expand_carrot_toggles(2)
+        self.expand_carrot_toggles(3)
+        self.expand_carrot_toggles(4)
+        self.expand_carrot_toggles(5)
+        self.expand_carrot_toggles(6)
+
+    def close_all(self):
+        self.py.get("button[title='Toggle']").click()
+
+    def expand_carrot_toggles(self, index):
         self.py.getx(f"(//button[@title='Toggle'])[{index}]").click()
 
     def check_checkbox(self, index):
         self.py.getx(f"(//span[@class ='rct-checkbox'])[{index}]").click()
 
-    def check_for_half_checked_status(self, index):
+    def check_for_checked_status(self, index):
+        """can check for "uncheck", "half-checked" or "icon-check" """
         return self.py.getx(f"(//span[@class ='rct-checkbox'])[{index}]").get_attribute('innerHTML')
+
+
 
 
--- elements/tests/test_checkbox.py
+++ elements/tests/test_checkbox.py
@@ -1,11 +1,26 @@
 def test_check_commands(elements, py):
     elements.checkboxes.go_to_demoqa_checkbox()
-    elements.checkboxes.expand_home_toggle_first_time()
-    elements.checkboxes.expand_toggles(2)
+    elements.checkboxes.expand_first_toggle()
+    elements.checkboxes.expand_carrot_toggles(2)
     elements.checkboxes.check_checkbox(4)
     assert py.get(".text-success").should().contain_text("commands")
-    assert "half-check" in elements.checkboxes.check_for_half_checked_status(1)
-    assert "half-check" in elements.checkboxes.check_for_half_checked_status(2)
+    assert "half-check" in elements.checkboxes.check_for_checked_status(1)
+    assert "half-check" in elements.checkboxes.check_for_checked_status(2)
 
+
+def test_check_for_saved_state_after_closing(elements, py):
+    elements.checkboxes.go_to_demoqa_checkbox()
+    elements.checkboxes.expand_all()
+    elements.checkboxes.check_checkbox(4)
+    elements.checkboxes.check_checkbox(15)
+    elements.checkboxes.close_all()
+    elements.checkboxes.expand_first_toggle()
+    assert "half-check" in elements.checkboxes.check_for_checked_status(1)
+    assert "half-check" in elements.checkboxes.check_for_checked_status(2)
+    assert "uncheck" in elements.checkboxes.check_for_checked_status(3)
+    assert "icon-check" in elements.checkboxes.check_for_checked_status(4)
+    assert "icon-check" in elements.checkboxes.check_for_checked_status(15)
+    assert "icon-check" in elements.checkboxes.check_for_checked_status(16)
+    assert "icon-check" in elements.checkboxes.check_for_checked_status(17)
 
 
