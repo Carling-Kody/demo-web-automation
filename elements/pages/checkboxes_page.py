e0ff13243a451e97204f09d62579c84b76b80c5d
Kody Carling <kodycarling@workfront.com>
2020-07-09 23:51:05 -0600

    created dictionary for checkbox indexing to make tests more readable.

--- elements/pages/checkboxes_page.py
+++ elements/pages/checkboxes_page.py
@@ -9,16 +9,31 @@ class Checkboxes:
     def go_to_demoqa_checkbox(self):
         self.py.visit('https://demoqa.com/checkbox')
 
+    def expand_all_button(self):
+        self.py.get("button[class='rct-option rct-option-expand-all']").click()
+
+    def collapse_all_button(self):
+        self.py.get("button[class='rct-option rct-option-collapse-all']").click()
+
+    def get_checkbox_titles(self):
+        labels = []
+        title = self.py.find("span[class ='rct-title']")
+        count = 0
+        for i in title:
+            labels.append(i.text().lower())
+        return labels
+
+    def create_checkbox_json(self):
+        label_list = self.get_checkbox_titles()
+        checkbox_dictionary = {}
+        count = 0
+        for item in label_list:
+            checkbox_dictionary[f"{item}"] = count
+            count += 1
+        return checkbox_dictionary
+
     def expand_first_toggle(self):
         self.py.get("button[title='Toggle']").click()
-
-    def expand_all(self):
-        self.expand_first_toggle()
-        self.expand_carrot_toggles(2)
-        self.expand_carrot_toggles(3)
-        self.expand_carrot_toggles(4)
-        self.expand_carrot_toggles(5)
-        self.expand_carrot_toggles(6)
 
     def close_all(self):
         self.py.get("button[title='Toggle']").click()
@@ -32,6 +47,9 @@ class Checkboxes:
     def check_for_checked_status(self, index):
         """can check for "uncheck", "half-checked" or "icon-check" """
         return self.py.getx(f"(//span[@class ='rct-checkbox'])[{index}]").get_attribute('innerHTML')
+
+    def check_success_message(self, name):
+        return self.py.getx(f"//span[contains(text(),'{name}')]").get_attribute('outerText')
 
 
 
--- elements/tests/test_checkbox.py
+++ elements/tests/test_checkbox.py
@@ -10,11 +10,11 @@ def test_check_commands(elements, py):
 
 def test_check_for_saved_state_after_closing(elements, py):
     elements.checkboxes.go_to_demoqa_checkbox()
-    elements.checkboxes.expand_all()
+    elements.checkboxes.expand_all_button()
     elements.checkboxes.check_checkbox(4)
     elements.checkboxes.check_checkbox(15)
-    elements.checkboxes.close_all()
-    elements.checkboxes.expand_first_toggle()
+    elements.checkboxes.collapse_all_button()
+    elements.checkboxes.expand_all_button()
     assert "half-check" in elements.checkboxes.check_for_checked_status(1)
     assert "half-check" in elements.checkboxes.check_for_checked_status(2)
     assert "uncheck" in elements.checkboxes.check_for_checked_status(3)
@@ -22,5 +22,19 @@ def test_check_for_saved_state_after_closing(elements, py):
     assert "icon-check" in elements.checkboxes.check_for_checked_status(15)
     assert "icon-check" in elements.checkboxes.check_for_checked_status(16)
     assert "icon-check" in elements.checkboxes.check_for_checked_status(17)
+    assert elements.checkboxes.check_success_message('commands') == 'commands'
+    assert elements.checkboxes.check_success_message('downloads') == 'downloads'
+    assert elements.checkboxes.check_success_message('wordFile') == 'wordFile'
+    assert elements.checkboxes.check_success_message('excelFile') == 'excelFile'
+
+
+def test_new(elements, py):
+    elements.checkboxes.go_to_demoqa_checkbox()
+    elements.checkboxes.expand_all_button()
+    test = elements.checkboxes.get_checkbox_titles()
+    print(test)
+    test1 = elements.checkboxes.create_checkbox_json()
+    print(test1)
+
 
 
