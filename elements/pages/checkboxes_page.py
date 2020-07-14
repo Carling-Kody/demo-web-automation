142224a72ad00a97e3c46029ac2be8b82e85caeb
Kody Carling <kodycarling@workfront.com>
2020-07-14 17:38:29 -0600

    Finished Checkbox tests

--- elements/pages/checkboxes_page.py
+++ elements/pages/checkboxes_page.py
@@ -18,15 +18,14 @@ class Checkboxes:
     def get_checkbox_titles(self):
         labels = []
         title = self.py.find("span[class ='rct-title']")
-        count = 0
         for i in title:
             labels.append(i.text().lower())
         return labels
 
-    def create_checkbox_json(self):
+    def checkbox_json(self):
         label_list = self.get_checkbox_titles()
         checkbox_dictionary = {}
-        count = 0
+        count = 1
         for item in label_list:
             checkbox_dictionary[f"{item}"] = count
             count += 1
@@ -35,21 +34,30 @@ class Checkboxes:
     def expand_first_toggle(self):
         self.py.get("button[title='Toggle']").click()
 
-    def close_all(self):
-        self.py.get("button[title='Toggle']").click()
-
     def expand_carrot_toggles(self, index):
         self.py.getx(f"(//button[@title='Toggle'])[{index}]").click()
 
-    def check_checkbox(self, index):
+    def check_checkbox(self, name):
+        index = self.checkbox_json()[name]
         self.py.getx(f"(//span[@class ='rct-checkbox'])[{index}]").click()
 
-    def check_for_checked_status(self, index):
+    def check_for_checked_status(self, name):
         """can check for "uncheck", "half-checked" or "icon-check" """
+        index = self.checkbox_json()[name]
         return self.py.getx(f"(//span[@class ='rct-checkbox'])[{index}]").get_attribute('innerHTML')
 
     def check_success_message(self, name):
         return self.py.getx(f"//span[contains(text(),'{name}')]").get_attribute('outerText')
+
+    def get_success_messages(self):
+        success_list = []
+        messages = self.py.find('.text-success')
+
+        for message in messages:
+            success_list.append(message.text())
+
+        return success_list
+
 
 
 
--- elements/tests/test_checkbox.py
+++ elements/tests/test_checkbox.py
@@ -11,30 +11,29 @@ def test_check_commands(elements, py):
 def test_check_for_saved_state_after_closing(elements, py):
     elements.checkboxes.go_to_demoqa_checkbox()
     elements.checkboxes.expand_all_button()
-    elements.checkboxes.check_checkbox(4)
-    elements.checkboxes.check_checkbox(15)
+    elements.checkboxes.check_checkbox('commands')
+    elements.checkboxes.check_checkbox('downloads')
     elements.checkboxes.collapse_all_button()
     elements.checkboxes.expand_all_button()
-    assert "half-check" in elements.checkboxes.check_for_checked_status(1)
-    assert "half-check" in elements.checkboxes.check_for_checked_status(2)
-    assert "uncheck" in elements.checkboxes.check_for_checked_status(3)
-    assert "icon-check" in elements.checkboxes.check_for_checked_status(4)
-    assert "icon-check" in elements.checkboxes.check_for_checked_status(15)
-    assert "icon-check" in elements.checkboxes.check_for_checked_status(16)
-    assert "icon-check" in elements.checkboxes.check_for_checked_status(17)
+    assert "half-check" in elements.checkboxes.check_for_checked_status('home')
+    assert "half-check" in elements.checkboxes.check_for_checked_status('desktop')
+    assert "uncheck" in elements.checkboxes.check_for_checked_status('notes')
+    assert "icon-check" in elements.checkboxes.check_for_checked_status('commands')
+    assert "icon-check" in elements.checkboxes.check_for_checked_status('downloads')
+    assert "icon-check" in elements.checkboxes.check_for_checked_status('word file.doc')
+    assert "icon-check" in elements.checkboxes.check_for_checked_status('excel file.doc')
     assert elements.checkboxes.check_success_message('commands') == 'commands'
     assert elements.checkboxes.check_success_message('downloads') == 'downloads'
     assert elements.checkboxes.check_success_message('wordFile') == 'wordFile'
     assert elements.checkboxes.check_success_message('excelFile') == 'excelFile'
+    assert 'commands' in elements.checkboxes.get_success_messages()
+    assert 'downloads' in elements.checkboxes.get_success_messages()
+    assert 'wordFile' in elements.checkboxes.get_success_messages()
+    assert 'excelFile' in elements.checkboxes.get_success_messages()
 
 
-def test_new(elements, py):
-    elements.checkboxes.go_to_demoqa_checkbox()
-    elements.checkboxes.expand_all_button()
-    test = elements.checkboxes.get_checkbox_titles()
-    print(test)
-    test1 = elements.checkboxes.create_checkbox_json()
-    print(test1)
+
+
 
 
 
